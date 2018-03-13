f8c6e3e70f41a2ab43c21de845dffac79d29851f
Ivuvar <https://Ivuvar@git.ttu.ee/ained/idk1011/ivuvar.git>
2018-03-13 19:24:16 +0200

    Praxi task

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -1,43 +1,45 @@
 package rental;
 
 public class RentalPriceCalculator {
+
+	public final double PRICE_UPPER_LIMIT = 1000.00;
 	
-	// age - age of driver
-	// licence - number of full years person holds driving licence
-	// clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
-	// acc - has s/he caused any accidents within last year
-	// acc2 - has s/he participated (but not caused) in any accidents within last year
-	// season - if it is high season or not
-	public double price(int age, int licence, int clazz, boolean acc, boolean acc2, boolean season) {
+	public double getRentalPrice(int driverAge, int yearsLicenceOwned, int carClass,
+								 boolean hasCausedAccidentsLastYear, boolean isHighSeason) {
 		
-		if (age < 18) {
+		if (driverAge < 18) {
 			throw new IllegalArgumentException("Driver too young - cannot quote the price");
 		}
-		if (age <= 21 && clazz > 2) {
+
+		if (driverAge <= 21 && carClass > 1) {
 			throw new UnsupportedOperationException("Drivers 21 y/o or less can only rent Class 1 vehicles");
 		}
-		
-		double rentalprice = age;
-		
-		if (clazz >=4 && age <= 25 && season != false) {
-			rentalprice = rentalprice * 2;
-		}
-		
-		if (licence < 1) {
+
+		if (yearsLicenceOwned < 1) {
 			throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
 		}
-		
-		if (licence < 3) {
-			rentalprice = rentalprice * 1.3;
-		}
-		
-		if (acc == true && age < 30) {
-			rentalprice += 15;
+
+		double rentalPrice = calculateRentalPrice(driverAge, yearsLicenceOwned, carClass, hasCausedAccidentsLastYear, isHighSeason);
+
+		return rentalPrice > PRICE_UPPER_LIMIT ? PRICE_UPPER_LIMIT : rentalPrice;
+	}
+
+	private double calculateRentalPrice(int driverAge, int yearsLicenceOwned, int carClass, boolean hasCausedAccidentsLastYear, boolean isHighSeason) {
+
+		double rentalPrice = driverAge;
+
+		if (carClass >= 4 && driverAge <= 25 && isHighSeason) {
+			rentalPrice *= 2;
 		}
 
-		if (rentalprice > 1000) {
-			return 1000.00;
+		if (yearsLicenceOwned < 3) {
+			rentalPrice *= 1.3;
 		}
-		return rentalprice;
+
+		if (hasCausedAccidentsLastYear && driverAge < 30) {
+			rentalPrice += 15;
+		}
+
+		return rentalPrice;
 	}
 }
--- test/rental/RentalPriceCalculatorTest.java
+++ test/rental/RentalPriceCalculatorTest.java
@@ -1,19 +1,52 @@
 package rental;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RentalPriceCalculatorTest {
-	
+
+	RentalPriceCalculator priceCalculator;
+
 	@Before
 	public void beforeEachTest() {
-		// this method is called before each test
+		priceCalculator = new RentalPriceCalculator();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDriverTooYoung() {
+		priceCalculator.getRentalPrice(17, 0, 1, false, false);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testLicenceNotOwnedForLongEnough() {
+		priceCalculator.getRentalPrice(18, 0, 1, false, false);
+
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void test21YearOldDrivingHigherClassCar() {
+		priceCalculator.getRentalPrice(21, 1, 2, false, false);
+
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAbsurdParametersDontExceedMaxPrice() {
+		Assert.assertEquals(priceCalculator.PRICE_UPPER_LIMIT, priceCalculator.getRentalPrice(10000, 1000, 50, true, true), 100);
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void test18YearOldCanOnlyRentClassOneCar() {
+		priceCalculator.getRentalPrice(18, 1, 2, false, false);
+	}
+
+	@Test
+	public void testLowestPriceIsDriverAge() {
+		int driverAge = 35;
+		double rentalPrice = priceCalculator.getRentalPrice(driverAge, 15, 1, false, false);
+		assertEquals((double) driverAge, rentalPrice, 0.5);
 	}
 }
